b06a0ed0659f4b9de913289fdc353f9752c472a2
Youngki Kang <yellowgg2@gmail.com>
2017-03-07 22:30:40 +0900

    add tasks to build the code

---
+++ README.md
@@ -0,0 +1,6 @@
+# React Todo List
+This is just for my experience.
+
+# test
+
+[Tell, don't ask](https://robots.thoughtbot.com/tell-dont-ask)
--- src/action/index.js
+++ src/action/index.js
@@ -2,17 +2,17 @@ import * as action from './action';
 
 let todoId = 0;
 export const addTodo = (text) => ({
-    type : action.ADD_TODO,
-    id : todoId++,
+    type: action.ADD_TODO,
+    id: todoId++,
     text
 })
 
 export const deleteTodo = (id) => ({
-    type : action.DELETE_TODO,
+    type: action.DELETE_TODO,
     id
 })
 
 export const toggleTodo = (id) => ({
-    type : action.TOGGLE_TODO,
+    type: action.TOGGLE_TODO,
     id
 })
