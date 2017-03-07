51acb8f5882ff3623814312da0edcf3e2a6e7f6e
Youngki Kang <yellowgg2@gmail.com>
2017-03-07 20:05:17 +0900

    first commit

---
+++ index.ios.js
@@ -0,0 +1,4 @@
+import TodoApp from './src';
+import { AppRegistry } from 'react-native';
+
+AppRegistry.registerComponent('kang_todo', () => TodoApp);
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class TodoApp extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <View>
+                <Text> aaa </Text>
+            </View>
+        );
+    }
+}
+
+export default TodoApp;
