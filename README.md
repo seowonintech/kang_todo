d58971439cb211bf5a4f0d7ca2cf4bc6c6ac2284
Youngki Kang <yellowgg2@gmail.com>
2017-03-08 21:28:20 +0900

    improve reducer

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # React Todo List
-This is just for practice.
-There may be bunch of bugs in this code!!
+This is just for practice.  
+There may be bunch of bugs in this code!!  
 Be careful to fork it out haha
 
 # I really love to ..
-1. Learn something new whatever it is.
-2. Have fun with someone.
-3. Make autoscript for a game.
-4. Enjoy freedom.
-5. Enjoy meditation.
+- Learn something new whatever it is.
+- Have fun with someone.
+- Make autoscript for a game.
+- Enjoy freedom.
+- Enjoy meditation.
 
 # I really hate
-1. Our president.
+- Our president.
--- src/reducer/manipulateTodos.js
+++ src/reducer/manipulateTodos.js
@@ -1,7 +1,7 @@
 import * as action from '../action/action';
 
 const manipulateTodos = (state = [], action) => {
-  switch (action.type){
+  switch (action.type) {
     // the action parameter
     // should be same as the returned values from action functions
     // such as addTodo, deleteTodo and toggleTodo except for type
@@ -11,10 +11,23 @@ const manipulateTodos = (state = [], action) => {
         ...state,
         {
           // values returned from addTodo action such as id and text, but no type.
-          id
+          id: action.id,
+          text: action.text,
+          complete: false
         }
       ]
     case action.DELETE_TODO:
+      return state.filter(t => t.id !== action.id);
+
+    // complete or not
     case action.TOGGLE_TODO:
+      return state.map(t => {
+        if (t.id === action.id){
+          return {
+            ...state,
+            complete: !complete
+          }
+        }
+      });
   }
 }
